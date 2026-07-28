14ff8f9d4d93bc155681af4dc391f8968ca11d34
Guilherme Escobar <guilherme.c.silva18@aluno.senai.br>
2026-07-28 10:27:53 -0300

    Add files via upload

---
+++ jornada.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double extra;
+        double horasTrabalhadas;
+        double total;
+        double salarioHora;
+
+        Console.WriteLine("Quantas Horas você trabalha no mês??");
+        horasTrabalhadas = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Quanto você recebe por horas??");
+        salarioHora = double.Parse(Console.ReadLine());
+
+        if (horasTrabalhadas > 160)
+        {
+            total = (160 * salarioHora) + ((horasTrabalhadas - 160) * salarioHora * 1.5);
+        }
+
+        else { 
+          total = salarioHora * horasTrabalhadas;
+        }
+
+        Console.WriteLine("Salário total: R$" + total);
+
+    }
+}
